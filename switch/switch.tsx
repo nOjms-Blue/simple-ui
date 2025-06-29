f084ed29167dcb96fade969aa0ab04a474236d0c
ブルー <98825268+nOjms-Blue@users.noreply.github.com>
2025-06-29 13:35:37 +0900

    fix: change colors when hover or focus

--- switch/switch.tsx
+++ switch/switch.tsx
@@ -1,5 +1,5 @@
 import type { ButtonHTMLAttributes } from "react";
-import { useState } from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 
 import { cn } from "../utils";
 
@@ -9,33 +9,43 @@ export interface SwitchProps
 	onCheckedChange?: (value: boolean) => void;
 }
 
-export const Switch = (props: SwitchProps) => {
-	const { className, checked, onCheckedChange, onClick, ...rest } = props;
-	const [isChecked, setIsChecked] = useState(checked ?? false);
+export const Switch = forwardRef<HTMLButtonElement, SwitchProps>(
+	(props, ref) => {
+		const { className, checked, onCheckedChange, onClick, ...rest } = props;
+		const [isChecked, setIsChecked] = useState(checked ?? false);
+		const triggerRef = useRef<HTMLButtonElement>(null);
 
-	if (typeof checked === "boolean") {
-		if (checked !== isChecked) {
-			setIsChecked(checked);
+		useImperativeHandle(ref, () => triggerRef.current!);
+
+		if (typeof checked === "boolean") {
+			if (checked !== isChecked) {
+				setIsChecked(checked);
+			}
 		}
-	}
 
-	return (
-		<button
-			{...rest}
-			type="button"
-			className={cn(
-				"relative h-10 w-16 rounded-full bg-gray-300 border border-gray-300 transition-colors duration-200",
-				"after:absolute after:inset-y-0 after:left-1 after:my-auto after:block after:size-8 after:rounded-full after:bg-white after:transition-transform after:duration-200",
-				"data-[checked=true]:bg-blue-500 data-[checked=true]:after:left-auto data-[checked=true]:after:right-1",
-				className,
-			)}
-			onClick={(e) => {
-				const newValue = !isChecked;
-				setIsChecked(newValue);
-				onCheckedChange?.(newValue);
-				onClick?.(e);
-			}}
-			data-checked={isChecked}
-		></button>
-	);
-};
+		return (
+			<button
+				{...rest}
+				type="button"
+				className={cn(
+					"relative h-10 w-16 rounded-full bg-gray-300 border border-gray-300 cursor-pointer transition-colors duration-200",
+					"after:absolute after:inset-y-0 after:left-1 after:my-auto after:block after:size-8 after:rounded-full after:bg-white after:transition-transform after:duration-200",
+					"data-[checked=true]:bg-blue-500 data-[checked=true]:after:left-auto data-[checked=true]:after:right-1",
+					"hover:bg-gray-400 focus:bg-gray-400 data-[checked=true]:hover:bg-blue-400 data-[checked=true]:focus:bg-blue-400",
+					className,
+				)}
+				onClick={(e) => {
+					onClick?.(e);
+					triggerRef.current?.blur();
+
+					const newValue = !isChecked;
+					setIsChecked(newValue);
+					onCheckedChange?.(newValue);
+				}}
+				data-checked={isChecked}
+				ref={triggerRef}
+			></button>
+		);
+	},
+);
+Switch.displayName = "Switch";
